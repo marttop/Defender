a538a2592130b47e4efddd72c6fcbf5f53537f92
Marton Szuts <mszuts@localhost.localdomain>
2020-02-17 15:14:48 +0100

    Side menu update

--- src/side_menu.c
+++ src/side_menu.c
@@ -15,11 +15,13 @@ void init_side_menu(all_t *s_all)
     sfTexture_createFromFile("sprites/side_menu.png", NULL);
     sfSprite_setTexture(s_all->s_side_menu.sprite,
     s_all->s_side_menu.texture, sfTrue);
-    sfSprite_setPosition(s_all->s_side_menu.sprite, (sfVector2f){1920, 0});
+    sfSprite_setPosition(s_all->s_side_menu.sprite, (sfVector2f){1420, 0});
 }
 
 void display_side_menu(all_t *s_all)
 {
-    sfRenderWindow_drawSprite(s_all->s_game.window,
-    s_all->s_side_menu.sprite, NULL);
+    if (s_all->s_selected.on == 1) {
+        sfRenderWindow_drawSprite(s_all->s_game.window,
+        s_all->s_side_menu.sprite, NULL);
+    }
 }
