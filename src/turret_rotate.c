d57344c45472de92360f7356e238c8253020c125
Lorentz ROMEO <lorentz-andrea.romeo@epitech.eu>
2020-02-20 01:05:40 +0100

    New formula for turret rotation

--- src/turret_rotate.c
+++ src/turret_rotate.c
@@ -12,8 +12,8 @@ void rotate_turret_maths(all_t *s_all)
     if (s_all->s_turret != NULL) {
         turret_t *tmp = s_all->s_turret;
         while (tmp != NULL) {
-            float angle = atan2(s_all->s_tuto->pos.y - tmp->pos.y,
-                s_all->s_tuto->pos.x - tmp->pos.x);
+            float angle = atan2(s_all->s_tuto->pos.y - (tmp->pos.y + 60),
+                s_all->s_tuto->pos.x - (tmp->pos.x + 60));
             angle = angle * 180 / 3.14159265359;
             tmp->rotate = angle + 90;
             sfSprite_setRotation(tmp->sprite_c, tmp->rotate);
