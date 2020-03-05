6dde681bcb8e3d83557511c1dbf5d3ee19813af9
Lorentz ROMEO <lorentz-andrea.romeo@epitech.eu>
2020-03-05 16:29:30 +0100

    Change to LVL position on the side turret menu

--- src/interface/slider2.c
+++ src/interface/slider2.c
@@ -9,34 +9,48 @@
 
 void slider_off3(all_t *s_all)
 {
-    s_all->s_sell.pos.x += 50, sfSprite_setPosition(s_all->s_sell.sprite,
-    s_all->s_sell.pos), s_all->s_info_text.pos_txt.x += 50, s_all->s_info_text.
-    pos_title.x += 50, sfText_setPosition(s_all->s_info_text.castle_txt, s_all
-    ->s_info_text.pos_txt), sfText_setPosition(s_all->s_info_text.ground_txt,
-    s_all->s_info_text.pos_txt), sfText_setPosition(s_all->s_info_text.
-    spawner_txt, s_all->s_info_text.pos_txt), sfText_setPosition(s_all->
-    s_info_text.title_c, s_all->s_info_text.pos_title);
-    sfText_setPosition(s_all->s_info_text.title_g, s_all->s_info_text.
-    pos_title), sfText_setPosition(s_all->s_info_text.title_s, s_all->
-    s_info_text.pos_title), sfText_setPosition(s_all->s_info_text.title_o,
-    s_all->s_info_text.pos_title), sfText_setPosition(s_all->s_info_text.
-    support_txt, s_all->s_info_text.pos_txt);
+    s_all->s_sell.pos.x += 50;
+    sfSprite_setPosition(s_all->s_sell.sprite, s_all->s_sell.pos);
+    s_all->s_info_text.pos_txt.x += 50, s_all->s_info_text.pos_title.x += 50;
+    sfText_setPosition(s_all->s_info_text.castle_txt,
+        s_all->s_info_text.pos_txt);
+    sfText_setPosition(s_all->s_info_text.ground_txt,
+        s_all->s_info_text.pos_txt);
+    sfText_setPosition(s_all->s_info_text.spawner_txt,
+        s_all->s_info_text.pos_txt);
+    sfText_setPosition(s_all->s_info_text.title_c,
+        s_all->s_info_text.pos_title);
+    sfText_setPosition(s_all->s_info_text.title_g,
+        s_all->s_info_text.pos_title);
+    sfText_setPosition(s_all->s_info_text.title_s,
+        s_all->s_info_text.pos_title);
+    sfText_setPosition(s_all->s_info_text.title_o,
+        s_all->s_info_text.pos_title);
+    sfText_setPosition(s_all->s_info_text.
+        support_txt, s_all->s_info_text.pos_txt);
 }
 
 void slider_on3(all_t *s_all)
 {
-    s_all->s_sell.pos.x -= 50, sfSprite_setPosition(s_all->s_sell.sprite,
-    s_all->s_sell.pos), s_all->s_info_text.pos_txt.x -= 50, s_all->s_info_text.
-    pos_title.x -= 50, sfText_setPosition(s_all->s_info_text.castle_txt, s_all
-    ->s_info_text.pos_txt), sfText_setPosition(s_all->s_info_text.ground_txt,
-    s_all->s_info_text.pos_txt), sfText_setPosition(s_all->s_info_text.
-    spawner_txt, s_all->s_info_text.pos_txt), sfText_setPosition(s_all->
-    s_info_text.title_c, s_all->s_info_text.pos_title);
-    sfText_setPosition(s_all->s_info_text.title_g, s_all->s_info_text.
-    pos_title), sfText_setPosition(s_all->s_info_text.title_s, s_all->
-    s_info_text.pos_title), sfText_setPosition(s_all->s_info_text.title_o,
-    s_all->s_info_text.pos_title), sfText_setPosition(s_all->s_info_text.
-    support_txt, s_all->s_info_text.pos_txt);
+  s_all->s_sell.pos.x -= 50;
+    sfSprite_setPosition(s_all->s_sell.sprite, s_all->s_sell.pos);
+    s_all->s_info_text.pos_txt.x -= 50, s_all->s_info_text.pos_title.x -= 50;
+    sfText_setPosition(s_all->s_info_text.castle_txt,
+        s_all->s_info_text.pos_txt);
+    sfText_setPosition(s_all->s_info_text.ground_txt,
+        s_all->s_info_text.pos_txt);
+    sfText_setPosition(s_all->s_info_text.spawner_txt,
+        s_all->s_info_text.pos_txt);
+    sfText_setPosition(s_all->s_info_text.title_c,
+        s_all->s_info_text.pos_title);
+    sfText_setPosition(s_all->s_info_text.title_g,
+        s_all->s_info_text.pos_title);
+    sfText_setPosition(s_all->s_info_text.title_s,
+        s_all->s_info_text.pos_title);
+    sfText_setPosition(s_all->s_info_text.title_o,
+        s_all->s_info_text.pos_title);
+    sfText_setPosition(s_all->s_info_text.
+        support_txt, s_all->s_info_text.pos_txt);
 }
 
 void slider_on4(all_t *s_all)
